9e13ba303575d70970e3a3c5c68b17bd28fd60ec
sanqing33 <1395251710@qq.com>
2024-05-18 23:47:49 +0800

    修改数据库

---
+++ doraemon-vue/src/components/user/blog.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import axios from "axios";
+import { ref, reactive, onMounted } from "vue";
+import { InterfaceUrl } from "@/api";
+import { useStore } from "vuex";
+import { ElMessage } from "element-plus";
+
+const store = useStore();
+
+const userInfo = store.getters.getUserInfo;
+
+onMounted(() => {
+  axios
+    .post(InterfaceUrl + "/user/getUserBlogList", {
+      id: userInfo.id,
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      ElMessage.error("请求失败，请联系管理员。");
+    });
+});
+</script>
+
+<style lang="scss" scoped></style>
