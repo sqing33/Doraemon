b52a5b173702107716efa8f6ea4d15c808ef31b4
sanqing33 <143684867+sanqing33@users.noreply.github.com>
2024-05-26 00:45:31 +0800

    Update index.js

--- doraemon-nodejs/db/index.js
+++ doraemon-nodejs/db/index.js
@@ -3,7 +3,6 @@ const Redis = require("ioredis");
 
 const mysqlDb = mysql.createPool({
   host: "127.0.0.1",
-  // host: "101.34.255.5",
   user: "root",
   password: "123456",
   database: "doraemon",
@@ -11,9 +10,7 @@ const mysqlDb = mysql.createPool({
 
 const redisDb = new Redis({
   host: "127.0.0.1",
-  // host: "101.34.255.5",
   port: 6379,
-  // password: "123456",
 });
 
 module.exports = {
